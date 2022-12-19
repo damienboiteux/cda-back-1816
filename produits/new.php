cbd89b2f90d42da0193bf612b83da2f5c87dd195
damienboiteux.com <contact@damienboiteux.com>
2022-12-19 04:08:28 +0100

    vendredi 16/12 17h10

--- produits/new.php
+++ produits/new.php
@@ -1,7 +1,171 @@
 <?php require '../header.php'; ?>
 <?php require '../utils/bdd.php'; ?>
 
+<?php
+$sql = "SELECT * FROM categorie";
+$requete = $pdo->query($sql);
+$categories = $requete->fetchAll(PDO::FETCH_ASSOC);
 
-<h1>Nouveau produit</h1>
+// Fluent
+// $categories = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);  
+
+if (!empty($_POST)) {
+
+    extract($_POST);
+
+    // Catégorie
+    if (empty($categorie)) {
+        $error['categorie'] = "La catégorie est obligatoire";
+    }
+
+    if (empty($reference)) {
+        $error['reference'] = "La référence est obligatoire";
+    } elseif (!preg_match('/^[a-zA-Z0-9]{4}$/', $reference)) {
+        $error['reference'] = "Référence incorrecte";
+    } else {
+        $sql = "SELECT count(*) FROM produit WHERE reference = '" . $reference . "'";
+        $requete = $pdo->query($sql);
+        if ($requete->fetch(PDO::FETCH_COLUMN) != 0) {
+            $error['reference'] = "Cette référence est déjà connue";
+        }
+    }
+
+    if (empty($nom)) {
+        $error['nom'] = "Le nom est obligatoire";
+    } elseif (!preg_match('/^[a-zA-Z0-9 -\']{4,64}$/', $nom)) {
+        $error['nom'] = "Nom incorrect";
+    } else {
+        $sql = "SELECT count(*) FROM produit WHERE nom = '" . $nom . "'";
+        $requete = $pdo->query($sql);
+        if ($requete->fetch(PDO::FETCH_COLUMN) != 0) {
+            $error['nom'] = "Ce produit existe déjà";
+        }
+    }
+
+    if (empty($description)) {
+        $error['description'] = "La description est obligatoire";
+    } elseif (!preg_match('/^[a-zA-Z0-9 -\']{1,500}$/', $description)) {
+        $error['description'] = "Description incorrecte";
+    }
+
+    if (!empty($prix)) {
+        if (!preg_match('/^\d+(,|.\d{1,2})?$/', $prix)) {
+            $error['prix'] = "Prix incorrect";
+        }
+    }
+
+    if (!empty($stock)) {
+        if (!preg_match('/^\d*)?$/', $prix)) {
+            $error['prix'] = "Valeur incorrecte";
+        }
+    }
+
+    if (empty($_FILES['image']['name'])) {
+        $error['image'] = "L'image est obligatoire";
+    } elseif (empty($error)) {
+        if ($_FILES['image']['size'] > 256 * 1024) {
+            $error['image'] = "L'image est trop volumineuse";
+        }
+        if (!in_array($_FILES['image']['type'], ['image/jpeg', 'image/png'])) {
+            $error['image'] = "Ce format d'image n'est pas autorisé";
+        }
+
+        $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+        $fichier = $reference . '.' . $extension;
+        if (!move_uploaded_file($_FILES['image']['tmp_name'], '../uploads/produits/' . $fichier)) {
+            $error['image'] = "Le téléchargement n'a pas pu être effectué";
+        };
+    }
+
+    if (empty($error)) {
+        $sql = "INSERT INTO produit (reference, nom, description, prix, stock, image, id_categorie) VALUES (:ref, :nom, :desc, :prix, :stock, :image, :cat)";
+        try {
+            $pdo->prepare($sql)->execute([
+                "ref"   => $reference,
+                "nom"   => $nom,
+                "desc"  => $description,
+                "prix"  => empty($prix) ? 0 : $prix,
+                "stock" => empty($stock) ? 0 : $stock,
+                "image" => $fichier,
+                "cat"   => $categorie
+            ]);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+        $_SESSION['messages'][] = "Le produit a été ajouté";
+        header('Location: ./index.php');
+    }
+}
+
+?>
+
+<h1 class="my-4">Nouveau produit</h1>
+
+<form action="" method="POST" enctype="multipart/form-data">
+
+    <div class="form-group">
+        <label for="categorie" class="form-label">Catégorie</label>
+        <select name="categorie" id="categorie" class="form-select">
+            <option value="">-- Choisir une catégorie --</option>
+            <?php foreach ($categories as $option) { ?>
+                <option value="<?= $option['id']; ?>" <?= $option['id'] == $categorie ? 'selected' : ''; ?>><?= $option['libelle']; ?></option>
+            <?php } ?>
+        </select>
+        <?php if (!empty($error['categorie'])) { ?>
+            <small class="text-danger"><?= $error['categorie']; ?></small>
+        <?php } ?>
+    </div>
+
+    <div class="form-group mt-4">
+        <label for="reference" class="form-label">Référence</label>
+        <input type="text" name="reference" id="reference" class="form-control" value="<?= $reference ?? ''; ?>">
+        <?php if (!empty($error['reference'])) { ?>
+            <small class="text-danger"><?= $error['reference']; ?></small>
+        <?php } ?>
+    </div>
+
+    <div class="form-group mt-4">
+        <label for="nom" class="form-label">Nom du produit</label>
+        <input type="text" name="nom" id="nom" class="form-control" value="<?= $nom ?? ''; ?>">
+        <?php if (!empty($error['nom'])) { ?>
+            <small class="text-danger"><?= $error['nom']; ?></small>
+        <?php } ?>
+    </div>
+
+    <div class="form-group mt-4">
+        <label for="description" class="form-label">Description</label>
+        <textarea name="description" id="description" class="form-control"><?= $description ?? ''; ?></textarea>
+        <?php if (!empty($error['description'])) { ?>
+            <small class="text-danger"><?= $error['description']; ?></small>
+        <?php } ?>
+    </div>
+
+    <div class="form-group mt-4">
+        <label for="prix" class="form-label">Prix</label>
+        <input type="text" name="prix" id="prix" class="form-control" value="<?= $prix ?? ''; ?>" placeholder="0.00">
+        <?php if (!empty($error['prix'])) { ?>
+            <small class="text-danger"><?= $error['prix']; ?></small>
+        <?php } ?>
+    </div>
+
+    <div class="form-group mt-4">
+        <label for="stock" class="form-label">Stock</label>
+        <input type="text" name="stock" id="stock" class="form-control" value="<?= $stock ?? ''; ?>" placeholder="0">
+        <?php if (!empty($error['stock'])) { ?>
+            <small class="text-danger"><?= $error['stock']; ?></small>
+        <?php } ?>
+    </div>
+
+    <div class="form-group mt-4">
+        <label for="image" class="form-label">Image</label>
+        <input type="file" name="image" id="image" class="form-control">
+        <?php if (!empty($error['image'])) { ?>
+            <small class="text-danger"><?= $error['image']; ?></small>
+        <?php } ?>
+    </div>
+
+    <button class="btn btn-primary mt-3">Valider</button>
+
+</form>
 
 <?php require '../footer.php'; ?>
--- produits/show.php
+++ produits/show.php
@@ -1,7 +1,46 @@
 <?php require '../header.php'; ?>
 <?php require '../utils/bdd.php'; ?>
 
+<?php
+if (!empty($_GET['id'])) {
+
+    // $sql = "SELECT * FROM produit, categorie";
+    // $sql .= " WHERE produit.id=" . $_GET['id'];
+    // $sql .= " AND produit.id_categorie=categorie.id";
+
+    $sql = "SELECT * FROM produit";
+    $sql .= " INNER JOIN categorie ON produit.id_categorie=categorie.id";
+    $sql .= " WHERE produit.id=" . $_GET['id'];
+
+    $requete = $pdo->query($sql);
+    if (!$produit = $requete->fetch(PDO::FETCH_ASSOC)) {
+        header('Location: ./index.php');
+    }
+
+    extract($produit);
+}
+
+?>
+
 
 <h1>Détails d'un produit</h1>
+
+<p>Référence : <?= $reference; ?></p>
+<p>Nom : <?= $nom; ?></p>
+<p>Catégorie : <?= $libelle; ?></p>
+<p>Description : <?= $description; ?></p>
+<p>Prix : <?= $prix; ?> €</p>
+<p>Stock : <?= $stock; ?></p>
+<div class="col-6">
+    <?php if (!empty($image)) { ?>
+        <img src="../uploads/produits/<?= $image; ?>" class="img-fluid" />
+    <?php } else { ?>
+        <!-- <img src="https://via.placeholder.com/400x400?text=Aucune+Photo+Disponible" class="img-fluid" /> -->
+        <img src="../uploads/produits/placeholder.png" class="img-fluid" />
+
+    <?php } ?>
+</div>
+
+<a href="./index.php" class="btn btn-primary mt-4">Retour à la liste</a>
 
 <?php require '../footer.php'; ?>
